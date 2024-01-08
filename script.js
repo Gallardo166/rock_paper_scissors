b2a151756d9ad240f13128b7bcf94b4999967b1f
Gallardo Lee <leegallardo16@gmail.com>
2024-01-08 19:32:40 +0800

    Create a best-of-five system

--- script.js
+++ script.js
@@ -16,12 +16,14 @@ function getComputerChoice() {
     function win(playerSelection, computerSelection) {
     //return a text displaying "You win! {playerSelection} beats {computerSelection}"
         console.log(`You win! ${playerSelection} beats ${computerSelection}`);
+        playerScore += 1;
     };
 
 //create a function called lose that does the following
     function lose(playerSelection, computerSelection) {
     //return a text displaying "You lose! {computerSelection} beats {playerSelection}"
         console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
+        computerScore += 1;
     };
 
 //create a function playRound that takes playerSelection and computerSelection and does the following:
@@ -62,38 +64,60 @@ function playRound(playerSelection, computerSelection) {
     };
 }
 
-let playerSelection, computerSelection;
+let playerSelection, computerSelection, playerScore, computerScore;
 
-//set a sentry isTie (True) for the following while loop:
-let isTie = true;
-while (isTie) {
-    //set computerSelection to be a random choice using getComputerChoice()
-    computerSelection = getComputerChoice();
-    //set a sentry keepGoing (True) for the following while loop:
-    let keepGoing = true;
-        //while keepGoing is True,
-    while (keepGoing) {
-            //get user input for playerSelection and capitalise it
-        playerSelection = capitalize(prompt("What's your choice? Rock, paper or scissors?"))
-            //if playerSelection is one of rock, paper or scissors, set keepGoing to False
-        switch (playerSelection) {
-            case "Rock":
-            case "Paper":
-            case "Scissors":
-                keepGoing = false;
-                break;
-            //else, keepGoing is still True
-            default:
+//create a function called game() that calls playRound until either computer or player wins (using a while loop that stops when either playerScore or computerScore reaches 5)
+function game() {
+    //create a variable called playerScore and computerScore with initial values of 0 (when win() is called, playerScore is incremented by 1; when lose() is called, computerScore is incremented  by 1 => add these inside win and lose function declarations)
+    playerScore = 0;
+    computerScore = 0;
+    while (playerScore < 5 && computerScore < 5) {
+        //set a sentry isTie (True) for the following while loop:
+        let isTie = true;
+        while (isTie) {
+            //set computerSelection to be a random choice using getComputerChoice()
+            computerSelection = getComputerChoice();
+            //set a sentry keepGoing (True) for the following while loop:
+            let keepGoing = true;
+                //while keepGoing is True,
+            while (keepGoing) {
+                    //get user input for playerSelection and capitalise it
+                playerSelection = capitalize(prompt("What's your choice? Rock, paper or scissors?"))
+                    //if playerSelection is one of rock, paper or scissors, set keepGoing to False
+                switch (playerSelection) {
+                    case "Rock":
+                    case "Paper":
+                    case "Scissors":
+                        keepGoing = false;
+                        break;
+                    //else, keepGoing is still True
+                    default:
+                };
+            };
+            //if playerSelection is the same as computerSelection,
+            if (playerSelection === computerSelection) {
+                //print "It is a tie!" and isTie is still True
+                console.log("It is a tie!");
+            //else, set isTie to be False, and call playRound()
+            } else {
+                isTie = false;
+                playRound(playerSelection, computerSelection, playerScore, computerScore);
+            };
         };
-    };
-    //if playerSelection is the same as computerSelection,
-    if (playerSelection === computerSelection) {
-        //print "It is a tie!" and isTie is still True
-        console.log("It is a tie!")
-    //else, set isTie to be False, and call playRound()
+        console.log(`Player: ${playerScore}`);
+        console.log(`Computer: ${computerScore}`);
+    }
+    //after playerScore or computerScore reaches 5, print who won:
+    //if playerScore reaches 5 first, print "Congratulations! You won!"
+    if (playerScore === 5) {
+        console.log("Congratulations! You won!");
+        console.log(`Player: ${playerScore}`);
+        console.log(`Computer: ${computerScore}`);
+    //else computerScore reaches 5 first, print "Oh no! You lost!"
     } else {
-        isTie = false
-        playRound(playerSelection, computerSelection)
+        console.log("Oh no! You lost!");
+        console.log(`Player: ${playerScore}`);
+        console.log(`Computer: ${computerScore}`);
     };
 };
 
@@ -110,3 +134,23 @@ function capitalize(string) {
     //return the fist character and the remaining character as one piece
     return firstCharacter + remainingCharacters;
 };
+
+//induce an infinite loop for a game of best of five, that will only stop when player chooses to quit
+//create a sentry variable called playAgain (initial value true) for the following while loop:
+playAgain = true;
+//while playAgain is true,
+while (playAgain) {
+    //call game() to start a game
+    game();
+    //get user input whether they want to play again or not, storing it in playerContinue
+    playerContinue = prompt("Do you want to play again? (Yes/No)").toLowerCase();
+    //if playerContinue is "Yes",
+    if (playerContinue === "yes") {
+        //playAgain remains true
+        playAgain = true;
+    //if playerContinue is "No",
+    } else {
+        //set playAgain to false to exit the game
+        playAgain = false;
+    };    
+}
